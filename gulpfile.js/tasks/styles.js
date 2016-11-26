1eb370496ff37f1939655164ad6aa5c5510d7aae
Mickael Zhang <mickael.zhg@gmail.com>
2016-11-26 15:53:59 +0100

    Remove gulp-source-map package
    I didn't use it at all in my last project so I decided to remove it

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -4,14 +4,11 @@ var gulp         = require('gulp'),
 		sass         = require('gulp-sass'),
 		autoprefixer = require('gulp-autoprefixer'),
 		cssnano      = require('gulp-cssnano'),
-		sourcemaps   = require('gulp-sourcemaps'),
     config       = require('../../gulpconfig');
 
 gulp.task('styles', function() {
   /* Where scss file come from */
   return gulp.src( config.path.src + 'scss/**/*.scss' )
-  /* Init sourcemaps */
-  .pipe( sourcemaps.init() )
   /* SASS Config */
   .pipe( sass( {
     includePaths: [
@@ -28,7 +25,6 @@ gulp.task('styles', function() {
     browsers: ['> 3%', 'last 2 versions', 'ie 9', 'ios 6', 'android 4']
   } ) )
   .pipe( cssnano() )
-  .pipe( sourcemaps.write('./') )
   /* Where the dist file is put */
   .pipe( gulp.dest( config.path.build + 'css/' ) );
 });
