955aaf8d47053e6e3809d97f774616a6be5ee0c5
Mickael Zhang <mickael.zhg@gmail.com>
2017-07-03 23:51:13 +0200

    Change gulp task for JS, now support ES6

--- gulpfile.js/tasks/scripts.js
+++ gulpfile.js/tasks/scripts.js
@@ -1,41 +1,17 @@
 // ==== SCRIPTS ==== //
-
 var gulp     = require('gulp'),
-    jshint   = require('gulp-jshint'),
-		concat   = require('gulp-concat'),
-		uglify   = require('gulp-uglify'),
-  	config   = require('../gulpconfig');
+		browserify = require("browserify"),
+		babelify = require("babelify"),
+		source = require('vinyl-source-stream'),
+		gutil = require('gulp-util'),
+		config   = require('../gulpconfig');
 
-// Check core scripts for errors
-gulp.task('scripts-lint', function() {
-  /**
-   * Linting checks the quality of the code; we only lint custom scripts, not those under the various modules,
-   * so we're relying on the original authors to ship quality code
-   */
-  return gulp.src([
-    config.path.src + 'js/app/**/*.js',
-    config.path.src + 'js/main.js'
-  ])
-  .pipe(jshint())
-  .pipe(jshint.reporter('default')); // No need to pipe this anywhere
+gulp.task('scripts', function() {
+	browserify({ debug: true })
+		.transform(babelify)
+		.require(config.path.src+'js/main.js', { entry: true })
+		.bundle()
+		.on('error',gutil.log)
+		.pipe(source('main.js'))
+    .pipe(gulp.dest(config.path.build+'js'));
 });
-
-// Minify scripts in place
-gulp.task('scripts-minify', ['scripts-lint'], function(){
-  /* Order in which file are concatenate */
-  return gulp.src( [
-    config.path.src + 'js/_lib/**/*.js',
-    config.path.src + 'js/app/**/*.js',
-    config.path.src + 'js/main.js'
-  ] )
-  /* Name of the js file */
-  .pipe( concat( 'main.js' ) )
-  /* Where the file should be put */
-  .pipe( gulp.dest( config.path.build + 'js/' ) )
-  /* Uglify file */
-  .pipe( uglify( {} ) )
-  /* Where the file should be put */
-  .pipe( gulp.dest( config.path.build + 'js/' ) );
-});
-
-gulp.task('scripts', ['scripts-minify']);
